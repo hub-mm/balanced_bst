32dbac99f7079f179a80cce472ac4e654d75b80b
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-09-15 19:39:37 +0100

    More test for delete.

--- main.rb
+++ main.rb
@@ -12,6 +12,17 @@ tree.insert(10)
 tree.insert(2)
 tree.insert(109_292)
 tree.insert(0)
+tree.insert(45)
+tree.insert(50)
+tree.insert(6)
 puts "\n|-----------------------------|\n\n"
 tree.pretty_print
+
+tree.delete(109_292)
+tree.delete(67)
+tree.delete(4)
+tree.delete(9)
+puts "\n|-----------------------------|\n\n"
+tree.pretty_print
+
 puts "\n|-----------------------------|\n\n"
